5c4fcee55a8c9a336876fa9e7b873c54b15b801d
Patrick Stünkel <webminz@googlemail.com>
2024-10-19 16:37:17 +0200

    Updated build script

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.0")
+    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.9")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testCompileOnly("junit:junit:4.13")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.google.guava:guava:33.3.1-jre")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+}
+
+repositories {
+    mavenCentral()
+}
+
+
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
--- src/main/java/no/hvl/past/util/FileSystemUtils.java
+++ src/main/java/no/hvl/past/util/FileSystemUtils.java
@@ -1,7 +1,6 @@
 package no.hvl.past.util;
 
 
-
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
